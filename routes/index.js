07651e094419e54db518303c88867839d338a7e8
zhang96 <jzhan264@uottawa.ca>
2016-03-23 21:39:56 -0400

    index.js update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+// var pg = require('pg');
+// var connectionString = require(path.join(__dirname, '../', '../', 'config'));
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres:*******MY**PASSWORD*******@localhost:5433/postgres';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
