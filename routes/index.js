c5ba1b69a8e5bacc0829fa4e95c55ddab66fa810
zhang96 <jzhan264@uottawa.ca>
2016-04-06 23:57:46 -0400

    Intention is ready to go.

--- routes/index.js
+++ routes/index.js
@@ -14,20 +14,19 @@ router.get('/index', function (req, res, next) {
 
 //Load login page when the request were sent.
 router.post('/login', function (req, res, next) {
-    
+
     // Find the user using email and pass
     db.users.find({
             email: req.body.email
             , pass: req.body.password
         }
         , function (err, result) {
-        if (result[0] !== undefined){
-            
-            res.render('login');
-            
-        }else 
-            {
-             res.send('wrong pass');
+            if (result[0] !== undefined) {
+
+                res.render('login');
+
+            } else {
+                res.send('wrong pass');
             }
         });
 });
@@ -36,5 +35,16 @@ router.get('/signup', function (req, res, next) {
     res.render('signup');
 });
 
+router.post('/signup', function (req, res, next) {
+    console.log(req.body);
+    db.users.save(req.body, function (err, result) {
+        console.log(result);
+        console.log('user entered');
+    });
+});
+
+db.run("select * from users", function(err, res){
+console.log(res);
+});
 
 module.exports = router;
