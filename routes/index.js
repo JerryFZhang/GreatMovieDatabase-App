f81392a55250b1ad5f8f48c7bd8bdf6ce8fa9b4a
zhang96 <jzhan264@uottawa.ca>
2016-04-09 11:16:53 -0400

    Done

--- routes/index.js
+++ routes/index.js
@@ -36,15 +36,77 @@ router.get('/signup', function (req, res, next) {
 });
 
 router.post('/signup', function (req, res, next) {
-    console.log(req.body);
-    db.users.save(req.body, function (err, result) {
+    
+    var userObject = req.body;
+    var allUser;
+    var lastUser;
+    var thisuserIdNum;
+    var lastuserId;
+    var nextuserId;
+    var insertionObject;
+    
+    db.run("select * from users", function(err, result){
+        allUser = result;
+        console.log('All users');
         console.log(result);
-        console.log('user entered');
+        lastUser = allUser[allUser.length - 1];
+        console.log('The last user');
+        console.log(lastUser);
+        
+        lastuserId = lastUser.userid;
+  
+        console.log('lastuserId');
+        console.log(lastuserId);
+    
+        thisuserIdNum = parseInt(lastuserId.substr(1, 4)) + 1;
+    
+        console.log('thisuserIdNum');
+        console.log(thisuserIdNum);
+        
+        var my_string = '' + thisuserIdNum;
+        
+        while (my_string.length < 4) {
+        my_string = '0' + my_string;
+        }
+
+        nextuserId = 'U'+my_string;
+        console.log (nextuserId);
+        
+        insertionObject = {
+        userid:nextuserId,
+        pass: userObject.pw1,
+        first_name: userObject.firstname,
+        last_name:userObject.lastname,
+        email:userObject.email,
+        year_born:userObject.year,
+        gender:userObject.gender
+        };
+       
+        console.log(insertionObject);
+        
+        db.users.find('email ='+ userObject.email, function(err, emailValidation) {
+        console.log(emailValidation);
+            if (typeof emailValidation === 'undefined'){
+                db.users.insert(insertionObject, function (err, insertionResult) {       //console.log(result);
+                console.log('user entered');
+                db.users.find({email :userObject.email}, function (err, insertionChecking){
+                    console.log(insertionChecking);
+                if ( insertionChecing === []){
+                    res.send('internal error');
+                }else{
+                    res.render('login');
+                }
+                });});}
+            else{ 
+                res.send('this email exist');
+            } 
+    });
     });
 });
 
-db.run("select * from users", function(err, res){
-console.log(res);
+
+db.run("select * from users", function(err, result){
+console.log(result);
 });
 
 module.exports = router;
